e0003d7e1eece12bb2e2f52ae7fed10dcd0e92e2
Muslin Akkaraviwat <muslin.akk@gmail.com>
2018-08-19 22:26:58 +0700

    Add stock api calling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
@@ -14,12 +15,30 @@ app.post('/webhook', (req, res) => {
     reply(reply_token, msg)
     res.sendStatus(200)
 })
+
 app.listen(port)
 function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {lwIfAPK5C+0hfoIcSjTjw8IlaMuuVVDlFbiZbUZb1rngt6ZavKw2uTPXsVayF5KRuS8VR6ZjKAnEN7veRIWzDQOQly9LcOhAMN6Z81skFi70mVm2XOtvHfl8K05TqccU8hamC277MAnLh2CwYQ0CBAdB04t89/1O/w1cDnyilFU=}'
     }
+
+    var options = {
+	  host: url,
+	  port: 80,
+	  path: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=CPN.BK&interval=1min&apikey=ZIBM6AL9W01TSKZH',
+	  method: 'GET'
+	};
+
+	http.request(options, function(res) {
+	  console.log('STATUS: ' + res.statusCode);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  res.setEncoding('utf8');
+	  res.on('data', function (chunk) {
+	    console.log('BODY: ' + chunk);
+	  });
+	}).end();
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -35,3 +54,5 @@ function reply(reply_token, msg) {
         console.log('status = ' + res.statusCode);
     });
 }
+
+
