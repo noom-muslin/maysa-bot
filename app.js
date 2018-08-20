1d3f3e794208bb32dc0bf4081495d9f1a1753a84
Muslin Akkaraviwat <muslin.akk@gmail.com>
2018-08-20 23:27:22 +0700

    Use axios

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 // Reply with two static messages
 const express = require('express')
-const https = require('https');
-const http = require('http')
 const bodyParser = require('body-parser')
-const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
 const URL = require('url').URL;
 const _ = require('lodash-node/compat');
+const axios = require('axios');
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,7 +13,7 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-
+    getStockData(msg)
     console.log(req)
     reply(reply_token, msg)
     res.sendStatus(200)
@@ -23,29 +21,23 @@ app.post('/webhook', (req, res) => {
 
 app.listen(port)
 function reply(reply_token, msg) {
-    const options = new URL('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+msg+'.BK&apikey=ZIBM6AL9W01TSKZH');
-	https.request(options, function(res) {
-	  console.log('STATUS: ' + res.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  res.setEncoding('utf8');
+    getStockData(msg).then(response => sendMessage(response, reply_token));
+}
 
-	  var responseString = '';
-	  res.on('data', function(data) {
-	    responseString += data;
-	  });
-
-	  res.on('end', function() {
-	    console.log(responseString);
-	    let responseObject = JSON.parse(responseString);
-	    let result = responseObject["Time Series (Daily)"];
-	    let firstElementKey = Object.keys(result)[0];
-	    console.log("NOOM: "+JSON.stringify(firstElementKey));
-	    console.log("NOOM: "+JSON.stringify(result[firstElementKey]));
-	    let response = JSON.stringify(firstElementKey)+"/n"+JSON.stringify(result[firstElementKey]);
-	    sendMessage(response,reply_token)
-	  });
-	}).end();
-
+function getStockData(msg){
+    axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+msg+'.BK&apikey=ZIBM6AL9W01TSKZH')
+    .then(response => {
+        console.log('STATUS: ' + response.statusCode);
+    	console.log('HEADERS: ' + JSON.stringify(response.headers));
+        console.log('DATA: ' + JSON.stringify(response.data));
+        let firstElement = response.data["Time Series (Daily)"]
+        let firstElementKey = Object.keys(firstElement)[0]
+        let result = JSON.stringify(firstElementKey)+" "+JSON.stringify(firstElement[firstElementKey])
+        return result;
+    })
+    .catch(error => {
+        console.log(error);
+    }).;
 }
 
 function sendMessage(response,reply_token){
@@ -53,6 +45,7 @@ function sendMessage(response,reply_token){
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {lwIfAPK5C+0hfoIcSjTjw8IlaMuuVVDlFbiZbUZb1rngt6ZavKw2uTPXsVayF5KRuS8VR6ZjKAnEN7veRIWzDQOQly9LcOhAMN6Z81skFi70mVm2XOtvHfl8K05TqccU8hamC277MAnLh2CwYQ0CBAdB04t89/1O/w1cDnyilFU=}'
     }
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -68,34 +61,3 @@ function sendMessage(response,reply_token){
         console.log('status = ' + res.statusCode);
     });
 }
-// const options = new URL('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=CPN.BK&apikey=ZIBM6AL9W01TSKZH');
-// let responseObject
-// https.request(options, function(res) {
-//   console.log('STATUS: ' + res.statusCode);
-//   console.log('HEADERS: ' + JSON.stringify(res.headers));
-//   res.setEncoding('utf8');
-
-//   var responseString = '';
-//   res.on('data', function(data) {
-//     responseString += data;
-//   });
-  
-//   let response;
-//   res.on('end', function() {
-//     console.log(responseString);
-//     responseObject = JSON.parse(responseString);
-//     let result = responseObject["Time Series (Daily)"];
-//     let firstElementKey = Object.keys(result)[0];
-//     console.log("NOOM: "+JSON.stringify(firstElementKey));
-//     console.log("NOOM: "+JSON.stringify(result[firstElementKey]));
-//     response = JSON.stringify(firstElementKey)+"/n"+JSON.stringify(result[firstElementKey]);
-
-//     console.log("response"+response);
-//   });
-// }).end();
-
-
-
-
-
-
