ed1b60534b255739bc53bef56684a7b38d157bcb
Muslin Akkaraviwat <muslin.akk@gmail.com>
2018-12-26 16:28:26 +0700

    Fix app to use roundrobin on partitions.

--- index.js
+++ index.js
@@ -11,14 +11,23 @@ const port = process.env.PORT || config.port ;
 const KeyedMessage = kafka.KeyedMessage
 const HighLevelProducer = kafka.HighLevelProducer
 
+const options = {
+    requireAcks: 1,
+    ackTimeoutMs: 100,
+    partitionerType: 2
+}
+
+const client = new kafka.KafkaClient({kafkaHost: config.kafkaHost});
+const producer =  new HighLevelProducer(client, options);
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('hello world');
-    handleEvent(config)
+    handleEvent(JSON.parse('{"type":"message","replyToken":"9407d326aef742cfab5517c35662f7e7","source":{"userId":"Uafdea049ee01fdfe5085af9d88cb3512","type":"user"},"timestamp":1545811209919,"message":{"type":"text","id":"9073356552278","text":"DEMOOOOOOO"}}'));
   });
 
-app.post('/webhook', line.middleware(config), (req, res) => {
+app.post('/webhook', line.middleware(config), (req, res) => {s
     // req.body.events should be an array of events
     if (!Array.isArray(req.body.events)) {
       return res.status(500).end();
@@ -35,26 +44,25 @@ app.post('/webhook', line.middleware(config), (req, res) => {
   });
 
 function handleEvent(event) {
-    var client = new kafka.KafkaClient({kafkaHost: config.kafkaHost});
-    var producer =  new HighLevelProducer(client);
 
     var corelationId = config.appName+'-'+uuid();
-
     var eventMessage = new KeyedMessage(corelationId, event);
     var payloads = [
+        // { topic: config.topicName, messages: JSON.stringify(eventMessage) },
         { topic: config.topicName, messages: JSON.stringify(eventMessage) }
     ];
-
-    producer.on('ready', function () {
-        producer.send(payloads, function (err, data) {
-            console.log("SEND PAYLOAD:", JSON.stringify(payloads), " DATA:", data);
-        });
+    producer.send(payloads, function (err, data) {
+        console.log("SEND PAYLOAD:", JSON.stringify(payloads), " DATA:", data);
     });
-    
-    producer.on('error', function (err) {
-        console.log("ERROR:",err);
-    })
 }
+
+producer.on('ready', function () {
+    console.log("KAFKA IS READY");
+});
+
+producer.on('error', function (err) {
+    console.log("ERROR:",err);
+})
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
